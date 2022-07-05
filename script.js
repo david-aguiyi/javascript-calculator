8f3686db74b2e93e3577fd3ce2bbb06540eb869a
david-aguiyi <davidaguiyi9@gmail.com>
2022-07-05 16:18:27 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const calculator = () => {
+  let firstNumber = parseFloat(prompt("Enter first number", 0));
+  if (isNaN(firstNumber)) return alert("Invalid input");
+
+  let operator = prompt(
+    "Enter operator (available operations: +, -, *, /, %): ",
+    "+"
+  );
+  if (
+    operator !== "+" &&
+    operator !== "-" &&
+    operator !== "*" &&
+    operator !== "/"
+  )
+    return alert("Invalid input");
+
+  let secondNumber = parseFloat(prompt("Enter second number", 0));
+  if (isNaN(secondNumber)) return alert("Invalid input");
+
+  let result;
+
+  switch (operator) {
+    case "+":
+      result = firstNumber + secondNumber;
+      break;
+    case "-":
+      result = firstNumber - secondNumber;
+      break;
+    case "*":
+      result = firstNumber * secondNumber;
+      break;
+    case "/":
+      result = firstNumber / secondNumber;
+      break;
+    case "%":
+      result = firstNumber % secondNumber;
+      break;
+    default:
+      result = "Invalid operator/operand(s)";
+      return alert(result);
+  }
+
+  return alert(`${firstNumber} ${operator} ${secondNumber} = ${result}`);
+};
+
+calculator();
